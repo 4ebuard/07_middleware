9f3d477e81cf56c8918195df4d94df8062d889cd
4ebuard <yartspetr@gmail.com>
2023-06-01 20:08:04 +0300

    07 added authGuard

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,21 @@ const app = express()
 const port = process.env.PORT || 3300
 
 const blablaMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    
+    if (req.query.token === "123") {
+    next();
+    } else { res.send(401)
+    }
+}
+
+const authGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
     //@ts-ignore
     req.blabla = "Hello";
     next();
 }
 
 app.use(blablaMiddleware)
+app.use(authGuardMiddleware)
 
 app.get('/products', (req: Request, res: Response) => {
     //@ts-ignore
